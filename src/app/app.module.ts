72ebff9d688e51a02d4cbd211ee557763c415f62
Joe Cserko <j.cserko@gmail.com>
2021-06-22 15:38:55 +0200

    feat: start.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-// cmd: npm i @ngrx/store;
-// cmd: npm i @ngrx/effects;
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
