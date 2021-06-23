002d0d048e3696647faaaa1f1e223ffb07ca93a6
Joe Cserko <j.cserko@gmail.com>
2021-06-23 16:23:33 +0200

    fix: dfiles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// cmd: ng g c page/home;
-// cmd: ng g c page/nav;
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
