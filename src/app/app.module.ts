fbc4e779b402011b91f9032b3241611b1532326b
Joe Cserko <j.cserko@gmail.com>
2021-06-23 17:20:38 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './page/home/home.component';
-import { NavComponent } from './page/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
