8aca03e0b9010e3c6c80dcbfd0df588327cb0ccf
Joe Cserko <j.cserko@gmail.com>
2021-06-24 16:41:13 +0200

    l01 end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
+// cmd: ng g c page/nav;
+// cmd: ng g c page/home;
+// cmd: ng g c page/login;
+// cmd: ng g c page/users;
+// cmd: ng g c page/user-edit;
+// cmd: ng g c page/forbidden;
+// cmd: ng g service service/jwt-interceptor;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './page/nav/nav.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { UsersComponent } from './page/users/users.component';
+import { UserEditComponent } from './page/user-edit/user-edit.component';
+import { ForbiddenComponent } from './page/forbidden/forbidden.component';
+import { JwtInterceptorService } from './service/jwt-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    UsersComponent,
+    UserEditComponent,
+    ForbiddenComponent,
   ],
   imports: [
     BrowserModule,
@@ -16,7 +37,9 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
