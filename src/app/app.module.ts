54b9a789a36eab22278c7c2bc024522a734889a0
Joe Cserko <j.cserko@gmail.com>
2021-06-24 12:27:07 +0200

    l03 end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // test 1 without terminal.
 // test 2 wt.
+// test 3 wt.
+// cmd: echo 'test';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
