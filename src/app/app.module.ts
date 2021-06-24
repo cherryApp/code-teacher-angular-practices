985447b56379e2341c27a7c6cc3f8a3de18f8ee8
Joe Cserko <j.cserko@gmail.com>
2021-06-24 16:24:57 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// test 1 without terminal.
-// test 2 wt.
-// test 3 wt.
-// cmd: echo 'test';
-// cmd: echo 'test222';
-// cmd: npm i;
-// hello world;
-// hello selection;
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
