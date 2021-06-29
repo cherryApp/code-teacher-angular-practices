f2a49b9090d6113ba22aae1943e3edf7c229bdf7
Joe Cserko <j.cserko@gmail.com>
2021-06-29 18:40:22 +0200

    l02 end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { FormsModule } from '@angular/forms';
 
 // cmd: ng g c page/home;
 // cmd: ng g c page/users;
-
+// cmd: ng g c page/nav;
+// cmd: ng g c page/forbidden;
+// cmd: ng g service service/jwt-interceptor;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
 import { UsersComponent } from './page/users/users.component';
+import { NavComponent } from './page/nav/nav.component';
+import { ForbiddenComponent } from './page/forbidden/forbidden.component';
+import { JwtInterceptorService } from './service/jwt-interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     UsersComponent,
+    NavComponent,
+    ForbiddenComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +31,9 @@ import { UsersComponent } from './page/users/users.component';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
