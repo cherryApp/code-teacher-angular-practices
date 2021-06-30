18449ff6efe31de51cedbae21106b599ef89fe33
Joe Cserko <j.cserko@gmail.com>
2021-06-30 14:12:46 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-// cmd: ng g c page/nav;
-// cmd: ng g c page/home;
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './page/nav/nav.component';
-import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
