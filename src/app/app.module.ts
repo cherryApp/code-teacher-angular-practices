5e2701264b8cc0d018e6ea29a15f4546c491e1fc
Joe Cserko <j.cserko@gmail.com>
2021-07-06 12:12:10 +0200

    l02 end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { FormsModule } from '@angular/forms';
 // cmd: ng g c page/forbidden;
 // cmd: ng g service service/jwt-interceptor;
 
+// cmd: ng g c page/test1;
+// cmd: ng g c page/test2;
+// cmd: ng g c page/test3;
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './page/nav/nav.component';
@@ -20,6 +24,9 @@ import { UsersComponent } from './page/users/users.component';
 import { UserEditComponent } from './page/user-edit/user-edit.component';
 import { ForbiddenComponent } from './page/forbidden/forbidden.component';
 import { JwtInterceptorService } from './service/jwt-interceptor.service';
+import { Test1Component } from './page/test1/test1.component';
+import { Test2Component } from './page/test2/test2.component';
+import { Test3Component } from './page/test3/test3.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +37,9 @@ import { JwtInterceptorService } from './service/jwt-interceptor.service';
     UsersComponent,
     UserEditComponent,
     ForbiddenComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
   ],
   imports: [
     BrowserModule,
